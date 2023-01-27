6ce39e7ff3c4bda72fcb95dafbb937e58ae4186c
Khayy01 <Kayodealuko77@gmail.com>
2023-01-27 16:54:40 +0100

    used windows as globals

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,18 +1,11 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
-
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-var helloSpeaker = {
-  speak:function speak(name) {
-    console.log(speakWord + " " + name);
-  }  
-}
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-var speakWord = "Hello";
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
@@ -23,3 +16,14 @@ var speakWord = "Hello";
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
+
+(function (window) {
+  var speakWord = "Hello";
+  var helloSpeaker = {
+    speak: function (name) {
+      console.log(speakWord + " " + name);
+    },
+  };
+
+  window.helloSpeaker = helloSpeaker;
+})(window);
